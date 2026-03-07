31196939d91994494e20d46956b3b1f3e1a1cc9a
Port Zhang <achui_1980@163.com>
2026-03-07 12:18:49 +0800

    Initial Commit

---
+++ OpenRightClick/OpenRightClickApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenRightClickApp.swift
+//  OpenRightClick
+//
+//  Created by achui on 2026/3/7.
+//
+
+import SwiftUI
+
+@main
+struct OpenRightClickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
